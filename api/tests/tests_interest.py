88e12e9c10b3520a8e414374bc0e6203bf0e55e8
shobu <shobu13@hotmail.fr>
2018-10-23 12:32:07 +0200

    places

--- api/tests/tests_interest.py
+++ api/tests/tests_interest.py
@@ -1,4 +1,9 @@
+import json
+
+from django.forms import model_to_dict
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
 from rest_framework.test import APIClient, APITestCase
 
@@ -17,11 +22,50 @@ class InterestViewsetTest(APITestCase):
     @classmethod  # <- setUpClass doit être une méthode de classe, attention !
     def setUpTestData(cls):
         super_user = User.objects.create_superuser(username='admin', password='sysadmin', email='')
-        Interest.objects.create('test')
+        Interest.objects.create(name='test')
 
     def setUp(self):
         self.super_user = User.objects.get(username='admin')
         self.client = APIClient()
 
-    def test_base(self):
-        pass
+        self.interest = Interest.objects.get(name='test')
+
+    def test_endpoint_list(self):
+        url = reverse('interest-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
+        interest_list = json.loads(response.content)
+
+        for interest in interest_list:
+            id = interest.get('id')
+            interest_model = InterestSerializer(model_to_dict(Interest.objects.get(id=id))).data
+            print(interest)
+            print(interest_model)
+            print("-------------")
+            self.assertEqual(interest, interest_model)
+
+    def test_endpoint_create(self):
+        self.assertTrue(self.client.login(username='admin', password='sysadmin'))
+        url = reverse('interest-list')
+        data = {
+            'name': 'test2',
+        }
+
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        test_interest = Interest.objects.get(name='test2')
+        self.assertTrue(test_interest)
+
+    def test_endpoint_delete(self):
+        self.assertTrue(self.client.login(username='admin', password='sysadmin'))
+
+        url = reverse('interest-detail', args=[self.interest.id])
+        response = self.client.delete(url, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, 204)
+
+        url = reverse('interest-list')
+        response = self.client.get(url, format='json')
+        print(response.data)
+        self.assertEqual(response.data, [])
+
--- places/models.py
+++ places/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=30)
-    latitude = models.CharField(max_length=100)
-    longitude = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
