a33c2bc439eb86deed5546dcf6b264fd2f0f0c65
Unknown <shobu13@hotmail.fr>
2018-11-22 14:27:41 +0100

    debug settings

--- DCG_API/settings.py
+++ DCG_API/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '+yuq1xm@sc4wwqf75chmnvcsjj!*b9wdhx9jb#c5pa9qbqzhw0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
