2f50c0c68c636f87ecf88d113dd50e7378db0e7e
shobu <shobu13@hotmail.fr>
2018-12-09 23:39:06 +0100

    bugfix

--- DCG_API/urls.py
+++ DCG_API/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf.urls.static import static
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token,
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from DCG_API import settings
 
