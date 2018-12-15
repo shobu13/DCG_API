a981a0327379baeeaf0535ba075a050589c74a3d
shobu <shobu13@hotmail.fr>
2018-12-15 20:40:13 +0100

    add nested serialisation for Event

--- api/serializers/event.py
+++ api/serializers/event.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 
 from event.models import Event
 
+from api.serializers import user
+
 
 class EventSerializer(serializers.ModelSerializer):
+    owner = user.UserSerializer()
+    participant = user.UserSerializer(many=True)
+
     class Meta:
         model = Event
         fields = '__all__'
